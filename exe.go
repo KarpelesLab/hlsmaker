9147d258b837a0f7f0ede9b607646054e19ffa30
Mark Karpeles <magicaltux@gmail.com>
2024-01-20 16:27:33 +0900

    allow commandline override of ffmpeg path

--- exe.go
+++ exe.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"path/filepath"
 )
 
+var (
+	ffmpegBin = flag.String("ffmpeg_bin", "", "path to ffmpeg executable")
+)
+
 func exe(n string) string {
+	if p := *ffmpegBin; p != "" {
+		// do a few attempts
+		a := filepath.Join(p, n)
+		if _, err := os.Stat(a); err == nil {
+			return a
+		}
+
+		// go back one level
+		p = filepath.Dir(p)
+
+		a = filepath.Join(p, n)
+		if _, err := os.Stat(a); err == nil {
+			return a
+		}
+
+		// fallback
+	}
+
 	// /pkg/main/media-video.ffmpeg.core/bin/...
 	p := filepath.Join("/pkg/main/media-video.ffmpeg.core/bin", n)
 	if _, err := os.Stat(p); err == nil {
